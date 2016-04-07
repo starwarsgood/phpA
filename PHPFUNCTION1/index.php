d4780578264abab163037ad8f83f5e9308fe482b
starwarsgood <comp822012016@mail.ru>
2016-04-07 17:37:55 +0300

    gggg

--- PHPFUNCTION1/index.php
+++ PHPFUNCTION1/index.php
@@ -1,10 +1,4 @@
 <?php
-
-$text = "\t\tIts PHP :) ";
-$binary = "\x09PHP\x0A";
-$hello  = "Hello, its my job";
-var_dump($text, $binary, $hello);
-$clean = ltrim($binary, "\x00..\x1F");
-
-
+$binary = '\x09PHP';
+echo strpbrk($binary, 'PHP');
 ?>
